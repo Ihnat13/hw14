ac9b26682bcfc2f5839016edae24ba70ba5a4d44
ihnat13 <ignatfomich7@gmail.com>
2024-01-18 08:36:04 +0200

    h14

---
+++ HW14.js
@@ -0,0 +1,34 @@
+const savedName = localStorage.getItem('username');
+const nameInputModal = document.getElementById('name-input-modal');
+const welcomeModal = document.getElementById('welcome-modal');
+const modalInput = document.querySelector(`.modal-content`);
+
+if (savedName) {
+    showWelcomeModal(savedName);
+} else {
+    showNameInputModal();
+}
+
+function showNameInputModal() {
+nameInputModal.style.display = 'block';
+document.body.classList.add('modal-open');
+}
+
+modalInput.addEventListener(`click`, ({ target }) => {
+    if(target.tagName === `BUTTON`){
+        saveName();
+    }
+})
+
+function saveName() {
+    const enteredName = document.getElementById('name-input').value;
+    localStorage.setItem('username', enteredName);
+    showWelcomeModal(enteredName);
+}
+
+function showWelcomeModal(username) {
+welcomeModal.style.display = 'block';
+document.getElementById('welcome-username').textContent = username;
+nameInputModal.style.display = 'none';
+document.body.classList.remove('modal-open');
+}
